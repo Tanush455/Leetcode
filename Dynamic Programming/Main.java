b8d38e4849a2af4d001381b27ef1a7d2a3489b81
Tanush Deshpande <tanushdeshpande73@gmail.com>
2026-01-01 22:28:09 +0530

    added the bottom up approach to the code as well

--- Dynamic Programming/Main.java
+++ Dynamic Programming/Main.java
@@ -37,11 +37,63 @@ class Solution {
     }
 }
 
+
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
         int[] arr = {40, 20, 30, 10, 30};
         System.out.println("Minimum number of multiplications is: "
                 + sol.matrixChainOrder(arr));
+    }
+}
+
+
+
+
+// Bottom up approach
+public class Solution {
+    public int matrixMultiplication(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+
+        // Initialize the dp array with large values
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        // A single matrix doesn't require any multiplication
+        for (int i = 1; i < n; i++) {
+            dp[i][i] = 0;
+        }
+
+        // Filling the dp array in a bottom-up manner
+        for (int length = 2; length < n; length++) {
+            for (int i = 1; i <= n - length; i++) {
+                int j = i + length - 1;
+                for (int k = i; k < j; k++) {
+                    // Calculate cost
+                    int cost = dp[i][k] + dp[k + 1][j] + nums[i - 1] * nums[k] * nums[j];
+                    // Take the minimum cost
+                    if (cost < dp[i][j]) {
+                        dp[i][j] = cost;
+                    }
+                }
+            }
+        }
+
+        // The result is in dp[1][n-1] (multiplying from matrix 1 to n-1)
+        return dp[1][n - 1];
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {10, 15, 20, 25};
+
+        // Output should be 8000
+        System.out.println("Minimum number of multiplications: " + sol.matrixMultiplication(nums));
     }
 }
